e42a1b2e15983ced1161acce3282f1537a0f35a2
Charlotte (Xinyu) Huang <xinyuhci@gmail.com>
2025-09-20 17:05:08 +0100

    breakyourwork

--- Interaction/Collection/Item3D.gd
+++ Interaction/Collection/Item3D.gd
@@ -1,20 +1,25 @@
 extends Area3D
 
-var collected := false
 var player_inside := false
+var collected := false
 
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)  
+	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 
 func _on_body_entered(body: Node) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and not collected:
 		player_inside = true
 
 func _on_body_exited(body: Node) -> void:
 	if body.is_in_group("player"):
 		player_inside = false
 
-func start_despawn_timer() -> void:
-	await get_tree().create_timer(2.0).timeout
-	self.visible = false
+func _unhandled_input(_event: InputEvent) -> void:
+	if collected and not player_inside:
+		print("retyur")
+	else:
+		print("gone")
+		if Input.is_action_just_pressed("interact"):   # E 键
+			await get_tree().create_timer(2.0).timeout
+			queue_free()  

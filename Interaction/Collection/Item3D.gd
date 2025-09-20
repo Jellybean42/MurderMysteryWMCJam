b609f47625b8ec2fa5e9abf907822f52d75a28c1
Charlotte (Xinyu) Huang <xinyuhci@gmail.com>
2025-09-20 16:29:46 +0100

    interact

--- Interaction/Collection/Item3D.gd
+++ Interaction/Collection/Item3D.gd
@@ -1,39 +1,20 @@
 extends Area3D
 
-@export var item_id: String = "paper_ball"
-@export var icon: Texture2D
 var collected := false
 var player_inside := false
 
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)   # Player 进入物品范围
+	body_entered.connect(_on_body_entered)  
 	body_exited.connect(_on_body_exited)
-	# 如果由 PickupTrigger 控制显隐，这里不需要再隐藏
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
 		player_inside = true
-		set_prompt_visible(true)
 
 func _on_body_exited(body: Node) -> void:
 	if body.is_in_group("player"):
 		player_inside = false
-		set_prompt_visible(false)
 
-func _unhandled_input(_e: InputEvent) -> void:
-	if not collected \
-	and player_inside \
-	and Interaction.player_in_zone \
-	and Input.is_action_just_pressed("interact"):
-		_collect()
-
-func _collect() -> void:
-	if collected: return
-	var ok :bool = Inventory.add_item(item_id, icon, true)
-	if ok:
-		collected = true
-		queue_free()
-
-func set_prompt_visible(v: bool) -> void:
-	if has_node("Label3D"):
-		$Label3D.visible = v
+func start_despawn_timer() -> void:
+	await get_tree().create_timer(2.0).timeout
+	self.visible = false
--- Interaction/Collection/PickupTrigger.gd
+++ Interaction/Collection/PickupTrigger.gd
@@ -1,15 +1,32 @@
 extends Area3D
-@export var item_node: Node3D   # 在 Inspector 里指向场景里的 Item3D
+
+@export var item_node: Node3D
 @export var one_shot := true
+
+var player_inside := false
+var fired := false
 
 func _ready() -> void:
 	monitoring = true
 	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
 	if item_node:
 		item_node.visible = false   # 初始隐藏
 
 func _on_body_entered(body: Node) -> void:
-	if body.is_in_group("player") and item_node:
-		item_node.visible = true
+	if body.is_in_group("player"):
+		player_inside = true
+
+func _on_body_exited(body: Node) -> void:
+	if body.is_in_group("player"):
+		player_inside = false
+
+func _unhandled_input(_event: InputEvent) -> void:
+	if fired or not player_inside:
+		return
+	if Input.is_action_just_pressed("interact"):   # E 键或你映射的按键
+		if item_node:
+			item_node.visible = true
+		fired = true
 		if one_shot:
 			monitoring = false
